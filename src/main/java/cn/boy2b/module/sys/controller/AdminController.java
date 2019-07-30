43cc74151bc8fbbce1962a35b37b83ba27ecf765
ant0209 <ant0209@163.com>
2019-07-30 18:49:36 +0800

    modify

--- src/main/java/cn/boy2b/module/sys/controller/AdminController.java
+++ src/main/java/cn/boy2b/module/sys/controller/AdminController.java
@@ -2,22 +2,21 @@ package cn.boy2b.module.sys.controller;
 
 import cn.boy2b.module.common.vo.ResultVO;
 import cn.boy2b.module.sys.entity.UserEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *@desc AdminController
  *@author zhouwei
  *@date 2019/7/29 17:08
  */
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class AdminController {
 
     @RequestMapping("/login")
-    @ResponseBody
-    public ResultVO login(UserEntity entity) {
+    public ResultVO login(@RequestBody UserEntity entity) {
         System.out.println(entity.getName());
         System.out.println(entity.getPassword());
         return ResultVO.createSuccess();
--- src/main/java/cn/boy2b/module/sys/controller/UserController.java
+++ src/main/java/cn/boy2b/module/sys/controller/UserController.java
@@ -1,14 +1,14 @@
 package cn.boy2b.module.sys.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zhouwei
  * @desc UserController
  * @date 2019-6-25 13:52
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
